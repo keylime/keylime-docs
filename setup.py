d4277ae8facaee85fec7b4bbebe95c7eb8037c3a
Luke Hinds <lhinds@redhat.com>
2019-04-13 15:46:23 +0100

    Fix rtd formatting

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup
 
-
-# Configuration is in setup.cfg
-setup()
+setup(name='keylime-docs',
+      version='0.0.3',
+      description='Documentation for the Nurtch platform',
+      url='https://github.com/keylime/keylime-docs',
+      author='Keylime Developers',
+      author_email='keylime@groups.io',
+      license='MIT',
+      zip_safe=False)
